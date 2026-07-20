7f9b556a6a516e8cd18773c50d37103b317eb699
Alef-0 <alefgabryel123@gmail.com>
2026-07-20 14:54:10 -0300

    Generate filters from central schema

--- menu_configurations.py
+++ menu_configurations.py
@@ -1,220 +1,164 @@
 import FreeSimpleGUI as sg
 
-def flatten_deep(nested_list):
-    flat_list = []
-    for item in nested_list:
-        # If the item is a list, recursively flatten it
-        if isinstance(item, list):
-            flat_list.extend(flatten_deep(item))
-        else:
-            flat_list.append(item)
-    return flat_list
+from ui.filter_schema import (
+    AMBIGUITY_STATE_OPTIONS,
+    DYNAMIC_PROPERTY_OPTIONS,
+    INVALID_STATE_OPTIONS,
+    PDH_KEY,
+)
+
 
 class Configurations:
     POWER = ["STANDARD", "-3dB Tx gain", "-6dB Tx gain", "-9dB Tx gain"]
     OUTPUT = ["NONE", "OBJECT", "CLUSTERS"]
     RCS = ["STANDARD", "HIGH SENSITIVITY"]
-    FONT = ("Helvetica", 12) 
+    FONT = ("Helvetica", 12)
 
     def __init__(self):
-        # Tem que colocar as configurações primeiro
         sg.set_options(font=self.FONT)
         sg.theme("SystemDefaultForReal")
-        
-        # Criar divisões especiais
         self.create_radar_division()
         self.create_options()
         self.create_filter_list()
-
-        self.layout = [ [self.FRAME], [sg.Push(), self.options, sg.Push()], [self.filter]]
+        self.layout = [[self.FRAME], [sg.Push(), self.options, sg.Push()], [self.filter]]
         self.window = sg.Window("Configurations Menu", self.layout, finalize=True)
-        
-        # PLEASE
-        # all_elements = flatten_deep(self.layout)
-        # print(all_elements)
-        
         for element in self.window.element_list():
-            # It checks every element across all rows and frames and fix their sizes
             if isinstance(element, sg.Combo):
-                combo_widget = element.Widget
-                popdown_name = combo_widget.tk.eval(f'ttk::combobox::PopdownWindow {combo_widget}')
-                combo_widget.tk.call(f'{popdown_name}.f.l', 'configure', '-font', ("Helvetica", 11) )
-                combo_widget.tk.call(f'{popdown_name}.f.l', 'configure', '-justify', 'center')
-
-        #Configurações adicionais
+                widget = element.Widget
+                popdown = widget.tk.eval(f"ttk::combobox::PopdownWindow {widget}")
+                widget.tk.call(f"{popdown}.f.l", "configure", "-font", ("Helvetica", 11))
+                widget.tk.call(f"{popdown}.f.l", "configure", "-justify", "center")
         self.centralize_combos()
         self.connected_radar = False
         self.connected_cam = False
 
     def create_radar_division(self):
-        """Criando Tabs para ficar mais organizado"""
-        GPS = sg.Frame("", 
-                       [[sg.Text("0° 0' 0\" N, 0° 0' 0\" E", expand_x=True, key="gps_text"), 
-                         sg.Button("OPEN GPS", key="conn_gps", button_color=("white", "green")), 
-                         sg.Button("MAPS", key="gps_maps")]], 
-                       title_location=sg.TITLE_LOCATION_RIGHT)
-        warning_text = [
-            sg.Push(), sg.Text("CLUSTER + QUALITY FOR GRAPHS"), sg.Push(), 
-            GPS, sg.Push()
-        ]
-        separation = []
-        names = ["", "LEFT", "MIDDLE", "RIGHT"]
-        letter = ["dsadas", "A", "B", "C"]
-        for i in range(1,4):
-            curr_tab = sg.Column([
-                [sg.Text(f"{names[i]} {letter[i]}", justification="center", expand_x=True)],
-                [sg.Text("Distance", expand_x=True, justification="left"),    sg.Text("XXX", key=f"DISTANCE_{i}", expand_x=True, justification="right")],
-                [sg.Text("Radar", expand_x=True, justification="left"),       sg.Text("XXX", key=f"RPW_{i}", expand_x=True, justification="right")],
-                [sg.Text("Output", expand_x=True, justification="left"),      sg.Text("XXX", key=f"OUT_{i}", expand_x=True, justification="right")],
-                [sg.Text("RCS", expand_x=True, justification="left"),         sg.Text("XXX", key=f"RCS_{i}", expand_x=True, justification="right")],
-                [sg.Text("Extra Info", expand_x=True, justification="left"),    sg.Text("XXX", key=f"EXT_{i}", expand_x=True, justification="right")],
-                [sg.Radio(f"Visualizar Grupo {letter[i]}", "visu_radar", key=f"choose_{i}", default= True if i == 2 else False, enable_events=True)]
-            ]
-            )
-            separation.extend([sg.Push(), curr_tab, sg.Push(), sg.VSep()])
-        separation.pop()
-
-        
-        self.FRAME = [sg.Frame("Real Time Configurations", [separation, warning_text], expand_x=True, title_location=sg.TITLE_LOCATION_TOP)]
+        gps = sg.Frame(
+            "",
+            [[
+                sg.Text("0° 0' 0\" N, 0° 0' 0\" E", expand_x=True, key="gps_text"),
+                sg.Button("OPEN GPS", key="conn_gps", button_color=("white", "green")),
+                sg.Button("MAPS", key="gps_maps"),
+            ]],
+            title_location=sg.TITLE_LOCATION_RIGHT,
+        )
+        warning = [sg.Push(), sg.Text("CLUSTER + QUALITY FOR GRAPHS"), sg.Push(), gps, sg.Push()]
+        columns = []
+        names = ("LEFT", "MIDDLE", "RIGHT")
+        letters = ("A", "B", "C")
+        for channel, (name, letter) in enumerate(zip(names, letters), start=1):
+            column = sg.Column([
+                [sg.Text(f"{name} {letter}", justification="center", expand_x=True)],
+                [sg.Text("Distance", expand_x=True), sg.Text("XXX", key=f"DISTANCE_{channel}", justification="right")],
+                [sg.Text("Radar", expand_x=True), sg.Text("XXX", key=f"RPW_{channel}", justification="right")],
+                [sg.Text("Output", expand_x=True), sg.Text("XXX", key=f"OUT_{channel}", justification="right")],
+                [sg.Text("RCS", expand_x=True), sg.Text("XXX", key=f"RCS_{channel}", justification="right")],
+                [sg.Text("Extra Info", expand_x=True), sg.Text("XXX", key=f"EXT_{channel}", justification="right")],
+                [sg.Radio(f"Visualizar Grupo {letter}", "visu_radar", key=f"choose_{channel}", default=channel == 2, enable_events=True)],
+            ])
+            columns.extend([sg.Push(), column, sg.Push()])
+            if channel != 3:
+                columns.append(sg.VSep())
+        self.FRAME = [sg.Frame("Real Time Configurations", [columns, warning], expand_x=True)]
 
     def create_options(self):
-        self.choices = sg.Frame("Radar", [[
-            sg.Radio("1", "choose", key="send_1"), sg.Radio("2", "choose", key="send_2"), 
-            sg.Radio("3", "choose", key="send_3"),  sg.Radio("all", "choose", key="send_all", default=True)
-        ]], 
-        title_location=sg.TITLE_LOCATION_LEFT)
-
-        self.column1 = sg.Column([
-            [sg.Checkbox("Radar Power", expand_x=True, key="CHECK_RPW",default=True), sg.Push(), sg.Combo(self.POWER, self.POWER[3], font=self.FONT, size=15, key="RPW", readonly=True), sg.Push()],
-            [sg.Checkbox("RCS Treshold", expand_x=True, key="CHECK_RCS",default=True),  sg.Combo(self.RCS, self.RCS[1], font=self.FONT, size=16, key="RCS", readonly=True), sg.Push()],
+        choices = sg.Frame("Radar", [[
+            sg.Radio("1", "choose", key="send_1"),
+            sg.Radio("2", "choose", key="send_2"),
+            sg.Radio("3", "choose", key="send_3"),
+            sg.Radio("all", "choose", key="send_all", default=True),
+        ]])
+        column1 = sg.Column([
+            [sg.Checkbox("Radar Power", key="CHECK_RPW", default=True), sg.Combo(self.POWER, self.POWER[3], key="RPW", readonly=True)],
+            [sg.Checkbox("RCS Threshold", key="CHECK_RCS", default=True), sg.Combo(self.RCS, self.RCS[1], key="RCS", readonly=True)],
         ])
-        self.column2 = sg.Column([
-            [sg.Checkbox("Output Power", expand_x=True, key="CHECK_OUT",default=True),sg.Push(), sg.Combo(self.OUTPUT, self.OUTPUT[2], font=self.FONT, size=15, key="OUT", readonly=True), sg.Push()],
-            [sg.Push(), sg.Checkbox("Send Quality", key="CHECK_QUALITY",default=True), sg.Push()]
+        column2 = sg.Column([
+            [sg.Checkbox("Output Type", key="CHECK_OUT", default=True), sg.Combo(self.OUTPUT, self.OUTPUT[2], key="OUT", readonly=True)],
+            [sg.Checkbox("Send Quality", key="CHECK_QUALITY", default=True)],
         ])
-
         self.options = sg.Frame("Options", [
-            # Distancia baseada no que o radar no modo consegue alcançar
-            [sg.Checkbox("Max Distance: ", key="CHECK_DISTANCE",default=True), sg.Text("196", size=(4, 1), font=("Helvetica", 12), key='SLIDER_VAL'), sg.Push(), 
-             sg.Slider((196, 260), 100, orientation="h", resolution=1, key="DISTANCE", size=(40, 20), disable_number_display=True, enable_events=True, expand_x=True), 
-             sg.Push()],
-            [self.column1, sg.VerticalSeparator(), self.column2],
-            [sg.Button("Send"), self.choices,  sg.VerticalSeparator(), sg.Button("OPEN RADAR", key="conn_radar", button_color=("white", "green")), sg.Button("OPEN CAM", key="conn_cam", button_color=("white", "green")),],
-            [sg.Button("SAVE in Non Volatile Memory", key="save_nvm", expand_x=True, button_color=("black", "white"))]
-        ], title_location=sg.TITLE_LOCATION_TOP)
-
-    def create_filter_list(self):
-        self.dynprop = sg.Column([
-            [sg.Push(), 
-                sg.Checkbox("Moving", default=True, enable_events=True, key="filter_dyn_move"),                     sg.Button("", button_color="#FF0000", disabled=True), 
-                sg.Checkbox("Stationary", default=True, enable_events=True, key="filter_dyn_stationary"),           sg.Button("", button_color="#FF7B00", disabled=True), 
-                sg.Checkbox("Oncoming", default=True, enable_events=True, key="filter_dyn_oncoming"),               sg.Button("", button_color="#FFE600", disabled=True), 
-                sg.Checkbox("Stationary Candidate",default=False, enable_events=True, key="filter_dyn_candidate"),   sg.Button("", button_color="#00FF00", disabled=True), 
-            sg.Push()],
-            [sg.Push(), 
-                sg.Checkbox("Unknown",default=True, enable_events=True, key="filter_dyn_unknown"),                  sg.Button("", button_color="#0000FF", disabled=True), 
-                sg.Checkbox("Crossing Stationary", default=True, enable_events=True, key="filter_dyn_crossstat"),   sg.Button("", button_color="#00FFFF", disabled=True), 
-                sg.Checkbox("Crossing Moving", default=True, enable_events=True, key="filter_dyn_crossmove"),       sg.Button("", button_color="#8400FF", disabled=True), 
-                sg.Checkbox("Stopped", default=True, enable_events=True, key="filter_dyn_stopped"),                 sg.Button("", button_color="#000000", disabled=True), 
-            sg.Push()],
-        ], justification="center")
-        
-        self.slider_pdh = sg.Column([
-            [sg.Push(), sg.Text("PDH - False Alarm Probability %\nZero is invalid", justification="center"), sg.Push()],
-            [sg.Slider((1, 7),disable_number_display=True, default_value=3, orientation='h', tick_interval=1, expand_x=True, enable_events=True, key="filter_phd")],
-            [sg.Text("[0, 1 = 25, 2 = 50, 3 = 75, 4 = 90, 5 = 99...]", expand_x=True, justification="center")]
-        ], justification="center")
-
-        self.cluster_ambg = sg.Column([
-            [sg.Push(), sg.Text("Ambiguitity State"), sg.Push()],
-            [
-                sg.Push(), sg.Checkbox("Ambiguous", enable_events=True, key="filter_ambg_ambig") , 
-                sg.Checkbox("Staggered Ramp", enable_events=True, key="filter_ambg_staggered"), sg.Push()
-            ],
-            [
-                sg.Push(), sg.Checkbox("Unambiguous", enable_events=True, default=True, key="filter_ambg_unambig"), 
-                sg.Checkbox("Stationary Candidates", default=True, enable_events=True, key="filter_ambg_stat") ,sg.Push()
-            ]
-        ], justification="center")
-
-        self.invalid_state = sg.Column([
-            [sg.Text("Cluster State", justification="center", expand_x=True)],
-            [sg.Push(), 
-                sg.Checkbox("0x0", default=True, enable_events=True,    key="filter_inv_00") , 
-                sg.Checkbox("0x1", enable_events=True,                  key="filter_inv_01"), 
-                sg.Checkbox("0x2", enable_events=True,                  key="filter_inv_02"), 
-                sg.Checkbox("0x3", enable_events=True,                  key="filter_inv_03"), 
-                sg.Checkbox("0x4", enable_events=True, default=True,    key="filter_inv_04") , 
-                sg.Checkbox("0x5", enable_events=True, disabled=True,   key="filter_inv_05"), 
-                sg.Checkbox("0x6", enable_events=True,                  key="filter_inv_06"), 
-                sg.Checkbox("0x7", enable_events=True,                  key="filter_inv_07"), 
-                sg.Checkbox("0x8", enable_events=True, default=True,    key="filter_inv_08") , 
-            sg.Push()],
-            [sg.Push(), 
-                sg.Checkbox("0x9", enable_events=True, default=True,    key="filter_inv_09"), 
-                sg.Checkbox("0xA", enable_events=True, default=True,    key="filter_inv_0A"), 
-                sg.Checkbox("0xB", enable_events=True, default=True,    key="filter_inv_0B"), 
-                sg.Checkbox("0xC", enable_events=True, default=True,    key="filter_inv_0C") , 
-                sg.Checkbox("0xD", enable_events=True, disabled=True,   key="filter_inv_0D"), 
-                sg.Checkbox("0xE", enable_events=True,                  key="filter_inv_0E"), 
-                sg.Checkbox("0xF", enable_events=True, default=True,    key="filter_inv_0F"),
-                sg.Checkbox("0x10", enable_events=True, default=True,   key="filter_inv_10") , 
-                sg.Checkbox("0x11", enable_events=True, default=True,   key="filter_inv_11"),  
-            sg.Push()],
+            [sg.Checkbox("Max Distance", key="CHECK_DISTANCE", default=True), sg.Text("196", key="SLIDER_VAL"),
+             sg.Slider((196, 260), 196, orientation="h", resolution=1, key="DISTANCE", disable_number_display=True, enable_events=True, expand_x=True)],
+            [column1, sg.VSep(), column2],
+            [sg.Button("Send"), choices, sg.VSep(),
+             sg.Button("OPEN RADAR", key="conn_radar", button_color=("white", "green")),
+             sg.Button("OPEN CAM", key="conn_cam", button_color=("white", "green"))],
+            [sg.Button("SAVE in Non Volatile Memory", key="save_nvm", expand_x=True, button_color=("black", "white"))],
         ], expand_x=True)
 
+    @staticmethod
+    def _option_control(option, include_color=False):
+        controls = [sg.Checkbox(option.label, key=option.key, default=option.default,
+                                disabled=not option.enabled, enable_events=True)]
+        if include_color:
+            controls.append(sg.Button("", button_color=option.color, disabled=True))
+        return controls
+
+    def create_filter_list(self):
+        dynamic_rows = []
+        for start in (0, 4):
+            row = [sg.Push()]
+            for option in DYNAMIC_PROPERTY_OPTIONS[start:start + 4]:
+                row.extend(self._option_control(option, include_color=True))
+            row.append(sg.Push())
+            dynamic_rows.append(row)
+        dynamic = sg.Column(dynamic_rows, justification="center")
+
+        pdh = sg.Column([
+            [sg.Text("PDH0 - False Alarm Probability (zero is invalid)", justification="center")],
+            [sg.Slider((1, 7), default_value=3, orientation="h", tick_interval=1,
+                       disable_number_display=True, expand_x=True, enable_events=True, key=PDH_KEY)],
+        ])
+
+        ambiguity = sg.Column([
+            [sg.Text("Ambiguity State", justification="center")],
+            [*sum((self._option_control(option) for option in AMBIGUITY_STATE_OPTIONS[:2]), [])],
+            [*sum((self._option_control(option) for option in AMBIGUITY_STATE_OPTIONS[2:]), [])],
+        ], justification="center")
+
+        invalid_rows = []
+        for start in (0, 9):
+            row = [sg.Push()]
+            for option in INVALID_STATE_OPTIONS[start:start + 9]:
+                row.extend(self._option_control(option))
+            row.append(sg.Push())
+            invalid_rows.append(row)
+        invalid = sg.Column([[sg.Text("Cluster Invalid State", expand_x=True, justification="center")], *invalid_rows], expand_x=True)
+
         self.filter = sg.Frame("Filters for points", [
-                [self.dynprop], 
-                [sg.HorizontalSeparator()], 
-                [self.slider_pdh, sg.VerticalSeparator(), self.cluster_ambg],
-                [sg.HorizontalSeparator()],
-                [self.invalid_state]
-            ], title_location=sg.TITLE_LOCATION_TOP, expand_x=True)
+            [dynamic], [sg.HorizontalSeparator()], [pdh, sg.VSep(), ambiguity],
+            [sg.HorizontalSeparator()], [invalid],
+        ], expand_x=True)
 
     def centralize_combos(self):
-        """Just Centralizing things messing with tkinter under the hood"""
-        self.window["RPW"].Widget.configure(justify="center")
-        self.window["OUT"].Widget.configure(justify="center")
-        self.window["RCS"].Widget.configure(justify="center")
+        for key in ("RPW", "OUT", "RCS"):
+            self.window[key].Widget.configure(justify="center")
 
     def read(self):
-        return self.window.read(1) # milliseconds
-    
+        return self.window.read(10)
+
     def change_connection_radar(self, connection):
-        self.connected_radar =  connection
-        if self.connected_radar:
-                self.window['conn_radar'].update("CLOSE RADAR", button_color=("white", "red"))
-        else:   self.window['conn_radar'].update("OPEN RADAR", button_color=("white", "green"))
+        self.connected_radar = connection
+        self.window["conn_radar"].update(
+            "CLOSE RADAR" if connection else "OPEN RADAR",
+            button_color=("white", "red" if connection else "green"),
+        )
+        for channel in range(1, 4):
+            self.change_radar({f"{key}_{channel}": "XXX" for key in ("DISTANCE", "RPW", "OUT", "RCS", "EXT")})
 
-        # Clear radar
-        def create_dict(channel):
-            dummy = {}
-            dummy[f'DISTANCE_{channel}'] = "XXX"
-            dummy[f'RPW_{channel}'] = "XXX"
-            dummy[f'OUT_{channel}'] = "XXX"
-            dummy[f'RCS_{channel}'] = "XXX"
-            dummy[f'EXT_{channel}'] = "XXX"
-            return dummy
-        
-        self.change_radar(create_dict(1))
-        self.change_radar(create_dict(2))
-        self.change_radar(create_dict(3))
-    
     def change_connection_cam(self, connection):
-        self.connected_cam =  connection
-        if self.connected_cam:
-                self.window['conn_cam'].update("CLOSE CAM", button_color=("white", "red"))
-        else:   self.window['conn_cam'].update("OPEN CAM", button_color=("white", "green"))
-    
+        self.connected_cam = connection
+        self.window["conn_cam"].update(
+            "CLOSE CAM" if connection else "OPEN CAM",
+            button_color=("white", "red" if connection else "green"),
+        )
+
     def change_connection_gps(self, connection):
-        if connection:  self.window['conn_gps'].update("CLOSE GPS", button_color=("white", "red"))
-        else:           self.window['conn_gps'].update("OPEN GPS", button_color=("white", "green"))
+        self.window["conn_gps"].update(
+            "CLOSE GPS" if connection else "OPEN GPS",
+            button_color=("white", "red" if connection else "green"),
+        )
 
-    def change_radar(self, dicio : dict):
-        for key, item in dicio.items():
-            self.window[key].update(item)
-            
-
-    def __del__(self): 
-        self.window.close()
-        print("DELETED WINDOW CONFIGURATION")
+    def change_radar(self, values):
+        for key, value in values.items():
+            self.window[key].update(value)
